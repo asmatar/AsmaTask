c6af22a87194d3ef0cd679f3013ed5ae87c1a7d1
Arthur Deruelle <arthur@MBP-de-Arthur.lan>
2024-04-02 14:38:46 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import { BrowserRouter as Router } from 'react-router-dom'
-import { UserAuthContextProvider } from './Context/AuthContext'
+import { UserAuthContextProvider } from '@/Context/AuthContext'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
 import { Provider } from 'react-redux'
